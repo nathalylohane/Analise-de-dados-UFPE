0233108fe651f7ad24d1ae9d15983e58155dcaff
Nathaly Lohane <nanelohane@gmail.com>
2023-04-24 11:06:37 -0300

    objeto, funcao e regressao

--- objeto e funcao.R
+++ objeto e funcao.R
@@ -1,5 +1,5 @@
 
 vetor1 <- c(1:10) #criação de um objeto simples de número naturais de 1 até 10
-str(vetor1) #verificação da complexidade do objeto por regressão
+str(vetor1) #verificação da complexidade do objeto 
 regcarros <- lm(speed ~ ., cars) #criação de um objeto complexo extraido da base cars
-str(regcarros) #verificação da complexidade do objeto por regressão
+str(regcarros) #verificação da complexidade do objeto 
