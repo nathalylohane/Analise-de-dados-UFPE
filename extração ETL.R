151ab59827743fc2d055cd91263ea616b1f02306
Nathaly Lohane <nanelohane@gmail.com>
2023-05-12 17:08:31 -0300

    Extração dos dados necessários

--- extração ETL.R
+++ extração ETL.R
@@ -1,4 +1,4 @@
-ls() # vai listar nossos objetos: "naZero", "sinistrosRecife2018Raw", "sinistrosRecife2019Raw" e "sinistrosRecifeRaw"
+ls() # vai listar nossos objetos: "naZero", "sinistrosRecife2020Raw", "sinistrosRecife2021Raw", "sinistrosRecife2022", "colunas_iguais", "colunas_iguais_2022" e "sinistrosRecifeRaw"
 
 # Analisando o quanto cada objeto está ocupando de espaço
 for (itm in ls()){
@@ -6,14 +6,16 @@ for (itm in ls()){
               format = "d",
               width = 30),
      quote = F   )
-} # o objeto da área intermediária que mais está ocupando espaço é o "sinistroRecife2018Raw" com 6202864
+} # o objeto da área intermediária que mais está ocupando espaço é o "sinistroRecife2020Raw" com 2765280
 
 gc()
 
 # Removendo os dados que não são mais úteis
-rm(list = c('sinistrosRecife2018Raw','sinistrosRecife2019Raw', "itm"))
+rm(list = c('sinistrosRecife2022Raw', 'sinistrosRecife2020Raw','itm',
+            'sinistrosRecife2021Raw', 'colunas_iguais', 'colunas_iguais_2022'))
 
 ls()
+
 
 saveRDS(sinistrosRecifeRaw,  "bases tratadas/sinistrosRecife.rds")
 write.csv2(sinistrosRecifeRaw, "bases tratadas/sinistrosRecife.cvs")
