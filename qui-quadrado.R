64208ddf2aacd9025fe603ac2423a0d2663d3a32
Nathaly Lohane <nanelohane@gmail.com>
2023-06-19 11:01:27 -0300

    teste de associação

---
+++ qui-quadrado.R
@@ -0,0 +1,24 @@
+## QUI-QUADRADO COM R ##
+# PRIMEIRO, VAMOS CARREGAR OS PACOTES
+pacman::p_load(data.table, ggplot2)
+
+# AGORA, A BASE DE DADOS 
+breast_cancer <- fread('https://raw.githubusercontent.com/hugoavmedeiros/cp_com_r/master/bases_tratadas/breast_cancer.csv', stringsAsFactors = T)
+breast_cancer <- fread('bases_tratadas/breast_cancer.csv', stringsAsFactors = T)
+View(breast_cancer)
+
+# TABELA DE CONTINGÊNCIA #
+breast_cancer_table <- table(breast_cancer$idade, breast_cancer$tumor_tamanho)
+breast_cancer_table
+
+# GRÁFICOS DE DISPERSÃO PAREADOS DAS VARIÁVEIS #
+ggplot(breast_cancer) + aes(x = tumor_tamanho, fill = idade) + geom_bar(position = "fill")
+
+# TESTE QUI QUADRADO #
+breast_cancer_test <- chisq.test(breast_cancer_table)
+breast_cancer_test
+breast_cancer_test$observed
+breast_cancer_test$expected
+
+# CORRPLOT DAS VARIÁVEIS #
+corrplot(breast_cancer_test$residuals, is.cor = FALSE)
